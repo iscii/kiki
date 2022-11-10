910630df40024fd91667f7d8a66002728eb1d3df
Issac <issac.zheng9869@gmail.com>
2022-11-10 10:04:07 -0500

    britney stuff

--- Assets/Scripts/waste_behavior.cs
+++ Assets/Scripts/waste_behavior.cs
@@ -10,6 +10,7 @@ public class waste_behavior : MonoBehaviour
     //gameObject variables
     public string desc;
     private BoxCollider2D col;
+    private bool in_range;
     // Start is called before the first frame update
     void Start()
     {
@@ -33,10 +34,18 @@ public class waste_behavior : MonoBehaviour
 
     private void OnMouseOver(){
         if (Input.GetMouseButtonDown(0)){
+            //!Issue -> only works for the the bounds of the collider, not when inside collider.
+            //Fix -> use OnCollisionEnter to set a flag in_range to true. 
+                //then on mouse button down, if in_range, we can destroy.
+                //would also allow us to easily deal with highlighting waste in range of player
             if (col.bounds.Intersects(player_collider.bounds)){
+                //remember to destroy waste colliders as well
                 Destroy(gameObject.GetComponent<Renderer>());
             }
             
         }
+    }
+    private void OnCollisionEnter(Collision other) {
+        
     }
 }
