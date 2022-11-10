b8e981288753f98641194aa24df27c98305c57b0
Issac <issac.zheng9869@gmail.com>
2022-11-09 21:49:33 -0500

    some random added stuff

--- Assets/Scripts/waste_behavior.cs
+++ Assets/Scripts/waste_behavior.cs
@@ -4,12 +4,15 @@ using UnityEngine;
 
 public class waste_behavior : MonoBehaviour
 {
+    //references
     private game_controller gc;
+    //gameObject variables
+    public string desc;
+
     // Start is called before the first frame update
     void Start()
     {
         gc = GameObject.Find("game").GetComponent<game_controller>();
-        
     }
 
     // Update is called once per frame
